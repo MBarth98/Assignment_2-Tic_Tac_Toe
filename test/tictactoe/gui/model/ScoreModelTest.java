52ce8b2ca15acdb1a74448b539c90424a6f24d60
kolobearpig <92294802+kolobearpig@users.noreply.github.com>
2021-10-11 10:52:43 +0200

    Update ScoreModelTest.java

--- test/tictactoe/gui/model/ScoreModelTest.java
+++ test/tictactoe/gui/model/ScoreModelTest.java
@@ -12,14 +12,38 @@ class ScoreModelTest {
     void setNextWinner() {
         ScoreModel instance = new ScoreModel();
         int expectedLength = 1;
-        String winner = "Peter";
+        String winner = winner;
 
         instance.setNextWinner(winner);
 
         int result = instance.getWinners().size();
         Assertions.assertEquals(expectedLength, result);
     }
+// Save player scores
+	this.scores = {
+		X: 0,
+		O: 0
+	};
 
+	this.marks = {
+		X: "X",  // Player 1 mark
+		O: "O",  // Player 2 mark
+		count: 0 // Number of moves made by player
+	};
+    function onResult(result, scores) {
+	if(result == 'draw') {
+		alert("It's a draw !");
+	} else {
+		alert(result + " has won");
+		updateScores(scores.X, scores.O);
+	}
+     	tictactoe.empty();
+}
+
+function updateScores(X, O) {
+	document.querySelector("#scoreboard #player1").innerHTML = X;
+	document.querySelector("#scoreboard #player2").innerHTML = O;	
+}   
     /**
      * Assert that the last winner is always placed at the first position in the list of winners.
      */
@@ -27,8 +51,8 @@ class ScoreModelTest {
     void setTwoWinners()
     {
         ScoreModel instance = new ScoreModel();
-        String winnerOne = "Peter";
-        String winnerTwo = "Jeppe";
+        String winnerOne = x;
+        String winnerTwo = o;
 
         instance.setNextWinner(winnerOne);
         instance.setNextWinner(winnerTwo);
